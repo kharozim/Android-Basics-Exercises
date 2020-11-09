c3dc92a0e1a6812a2bd92477bba0978dc37e6d8e
kharozim <kharozim06@gmail.com>
2020-11-10 06:48:53 +0700

    edit ThirdActivity.kt (add password condition)

--- app/src/main/java/id/refactory/andoridbasicapps/ThirdActivity.kt
+++ app/src/main/java/id/refactory/andoridbasicapps/ThirdActivity.kt
@@ -16,31 +16,30 @@ class ThirdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
-        var etUsername = findViewById<EditText>(R.id.et_email)
-        var etPassword = findViewById<EditText>(R.id.et_password)
+        val etUsername = findViewById<EditText>(R.id.et_email)
+        val etPassword = findViewById<EditText>(R.id.et_password)
         val btLogin = findViewById<Button>(R.id.bt_login)
 
-        var username = etUsername.text.toString()
-        var password = etPassword.text.toString()
-
         btLogin.setOnClickListener {
-
-            intentTo(HomeActivity::class.java)
-//            when{
-//                username.isEmpty() || password.isEmpty() -> Toast.makeText(applicationContext,"email / paswd kosong", Toast.LENGTH_SHORT).show()
-//                else -> intentTo(HomeActivity::class.java)
-//            }
-//            if (username.isEmailValid() && password.toString().isPasswordValid()) {
-//                val intent = Intent(this, HomeActivity::class.java)
-//                startActivity(intent)
+            when {
+                etUsername.text.toString().isEmailValid() && etPassword.text.toString().isPasswordValid() -> {
+                    intentTo(HomeActivity::class.java)
+                }
+                etPassword.text.toString().length <= 8 -> {
+                    etPassword.text.clear()
+                    etPassword.setError("minimum password 8 character")
+                }
+            }
         }
     }
-}
 
-//private fun String.isPasswordValid(): Boolean {
-//    return !TextUtils.isEmpty(this) && this.matches(Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~\$^+=<>]).{8,20}\$"))
-//}
-//
-//private fun String.isEmailValid(): Boolean {
-//    return !TextUtils.isEmpty(this) && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
-//}
+
+    private fun String.isPasswordValid(): Boolean {
+        return !TextUtils.isEmpty(this) && this.matches(Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~\$^+=<>]).{8,20}\$"))
+    }
+
+    private fun String.isEmailValid(): Boolean {
+        return !TextUtils.isEmpty(this) && android.util.Patterns.EMAIL_ADDRESS.matcher(this)
+            .matches()
+    }
+}
